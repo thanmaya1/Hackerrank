ac2585f43368799f923c77c9b5b06b4e4a4c5da0
Thanmaya Garimella <112673461+thanmaya1@users.noreply.github.com>
2023-01-02 07:41:33 +0530

    Create Advancedanalysis.c

---
+++ Advancedanalysis.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// Swaps the values at the given indices in the given array
+void swap(int arr[], int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+// Insertion sort function that returns the number of shifts performed
+int insertionSort(int arr[], int n) {
+    int shifts = 0;
+    for (int i = 1; i < n; i++) {
+        for (int j = i - 1; j >= 0; j--) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+                shifts++;
+            }
+        }
+    }
+    return shifts;
+}
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        int n;
+        scanf("%d", &n);
+        int arr[n];
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &arr[j]);
+        }
+        int shifts = insertionSort(arr, n);
+        printf("%d\n", shifts);
+    }
+    return 0;
+}
